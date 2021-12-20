b64fa25df2a3ffd9037dc9c387f9af7b73abd3b7
Hajime Kawahara <divrot@gmail.com>
2021-12-21 07:58:35 +0900

    start ts4

---
+++ src/ts4/__init__.py
@@ -0,0 +1,8 @@
+__all__ = []
+
+__version__ = "0.0.1"
+__uri__ = "http://secondearths.sakura.ne.jp/ts4/"
+__author__ = "Hajime Kawahara and collaborators"
+__email__ = "divrot@gmail.com"
+__license__ = "MIT"
+__description__ = "Test Sets for Spectral Signal Separation"
